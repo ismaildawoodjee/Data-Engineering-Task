92a98ec1c3e42c74f42c7b7880027bb9f990d2f2
Ismail Dawoodjee <ismail.dawoodjee27@gmail.com>
2021-06-02 12:14:04 +0800

    Testing out coins list request

--- ethprice.py
+++ ethprice.py
@@ -1,14 +1,24 @@
 import requests
-from pycoingecko import CoinGeckoAPI
+import json
 
 URL = "https://api.coingecko.com/api/v3"
 
 PING = "/ping"
+COINS_LIST = "/coins/list"
+SIMPLE_PRICE = "/simple/price"
 
 # try pinging the server
-response = requests.get(URL + PING)
-print(response.status_code, response.text)
+# response = requests.get(URL + PING)
+# print(response.status_code, response.text)
 
-cg = CoinGeckoAPI()
-print(cg.ping())
+def print_response(payload):
+    """Prints response from server based on HTTP request."""
+    
+    response = requests.get(URL + payload)
+    data = response.json()
+    
+    print(response.status_code)
+    print(json.dumps(data, indent=2))
 
+# print_response(payload=PING)
+print_response(payload=COINS_LIST)
